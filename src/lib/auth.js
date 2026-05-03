e011133cc1c2fe7798217f5dc820ddb2d1b400ad
Fabliha Naowar Nizam Deya <deyafabliha@gmail.com>
2026-05-03 15:14:05 +0600

    NO

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,21 +4,23 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.AUTH_DB_URL);
 
-// ✅ Add this
-await client.connect();
-
 const db = client.db();
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [
+    "https://qurbani-hat-8lyn.vercel.app",
+    "http://localhost:3000"
+  ],
   emailAndPassword: {
     enabled: true
   },
   socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID , 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET , 
-        }, 
-    },
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+    }, 
+  },
   database: mongodbAdapter(db, {
     client
   }),
