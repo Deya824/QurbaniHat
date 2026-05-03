59a744dc4c07103166f42ac0384e8f33470707f9
Fabliha Naowar Nizam Deya <deyafabliha@gmail.com>
2026-05-03 15:24:10 +0600

    e

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
 });
 
-// Destructure directly from the client you just created above
-export const { signIn, signUp, signOut , useSession } = createAuthClient();
+export const { signIn, signUp, signOut, useSession } = authClient;
